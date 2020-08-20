64acb97158493159ac206a9da0c4157e62990c4e
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-08-19 19:35:24 -0600

    Working. Added Zombie Class. Added Horde Creator. Zombie now spawn and move toward player.

---
+++ TextureHolder.cpp
@@ -0,0 +1,42 @@
+#include "TextureHolder.h"
+// Include the "assert feature"
+#include <assert.h>
+
+TextureHolder* TextureHolder::member_static_Instance = nullptr;
+
+TextureHolder::TextureHolder()
+{
+	assert(member_static_Instance == nullptr); //Make sure it is set to nullPtr. If it has something, assert will end the program. 
+	member_static_Instance = this;
+}
+
+Texture& TextureHolder::GetTexture(string const& filename)
+{
+	// Get a reference to m_Textures using m_s_Instance
+	auto& mapOfTextures = member_static_Instance->member_map_Textures;
+	// auto is the equivalent of map<string, Texture>
+	// Create an iterator to hold a key-value-pair (kvp)
+	// and search for the required kvp
+	// using the passed in file name
+	auto keyValuePair = mapOfTextures.find(filename);
+	// auto is equivalent of map<string, Texture>::iterator
+	// Did we find a match?
+	if (keyValuePair != mapOfTextures.end())
+	{
+		// Yes
+		// Return the texture,
+		// the second part of the kvp, the texture
+		return keyValuePair->second;
+	}
+	else
+	{
+		// File name not found
+		// Create a new key value pair using the filename
+		//Texture& texture = mapOfTextures[filename];  //mapOfTextures is of type map<string, Texture>
+		auto& texture = mapOfTextures[filename];  //mapOfTextures is of type map<string, Texture>		
+		// Load the texture from file in the usual way
+		texture.loadFromFile(filename);
+		// Return the texture to the calling code
+		return texture;
+	}
+}
--- ZombieSlaughter.cpp
+++ ZombieSlaughter.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include "Player.h"
 #include "ZombieArena.h"
+#include "TextureHolder.h"
 
 using namespace sf;
 
 int main()
 {
+	// Here is the instance of TextureHolder. Can only be created once, or an Assert will end the program.
+	TextureHolder holder;
+
 	// The game will always be in one of four states
 	enum class State {
 		PAUSED, LEVELING_UP,
@@ -46,6 +50,11 @@ int main()
 	// Load the texture for our background vertex array
 	Texture textureBackground;
 	textureBackground.loadFromFile("graphics/background_sheet.png");
+
+	// Prepare for a horde of zombies
+	int numZombies;
+	int numZombiesAlive;
+	Zombie* zombies = nullptr; //Will need memory maintenance
 
 	// The main game loop
 	while (window.isOpen())
@@ -175,6 +184,14 @@ int main()
 
 				// Spawn the player in the middle of the arena
 				player.spawn(arena, resolution, tileSize);
+
+				// Create a horde of zombies
+				numZombies = 10;
+				// Delete the previously allocated memory (if it exists)
+				delete[] zombies;
+				zombies = createHorde(numZombies, arena);
+				numZombiesAlive = numZombies;
+
 				// Reset the clock so there isn't a frame jump
 				clock.restart();
 			}
@@ -205,6 +222,15 @@ int main()
 			Vector2f playerPosition(player.getCenter());
 			// Make the view centre around the player
 			mainView.setCenter(player.getCenter());
+
+			// Loop through each Zombie and update them
+			for (int i = 0; i < numZombies; i++)
+			{
+				if (zombies[i].isAlive())
+				{
+					zombies[i].update(dt.asSeconds(), playerPosition);
+				}
+			}
 		}
 		// End updating the scene
 
@@ -222,6 +248,12 @@ int main()
 
 			// Draw the background
 			window.draw(background, &textureBackground);
+
+			// Draw the zombies
+			for (int i = 0; i < numZombies; i++)
+			{
+				window.draw(zombies[i].getSprite());
+			}
 
 			// Draw the player
 			window.draw(player.getSprite());
@@ -252,7 +284,8 @@ int main()
 	} 
 	//End Game Loop
 
-
+	// Delete the previously allocated memory (if it exists)
+	delete[] zombies;
 
 
 	return 0;
