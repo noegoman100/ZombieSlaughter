0ead87b97376cfcd0fa357aea11dac7bf5308c50
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-08-22 14:02:46 -0600

    Working. Added crosshair.

--- ZombieSlaughter.cpp
+++ ZombieSlaughter.cpp
@@ -66,6 +66,12 @@ int main()
 	// When was the fire button last pressed?
 	Time lastPressed;
 
+	// Hide the mouse pointer and replace it with crosshair
+	window.setMouseCursorVisible(true);
+	Sprite spriteCrosshair;
+	Texture textureCrosshair = TextureHolder::GetTexture("graphics/crosshair.png");
+	spriteCrosshair.setTexture(textureCrosshair);
+	spriteCrosshair.setOrigin(25, 25);
 
 	// The main game loop
 	while (window.isOpen())
@@ -268,8 +274,11 @@ int main()
 			// Where is the mouse pointer
 			mouseScreenPosition = Mouse::getPosition();
 			// Convert mouse position to world coordinates of mainView
-			mouseWorldPosition = window.mapPixelToCoords(
-				Mouse::getPosition(), mainView);
+			mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainView);
+
+			// Set the crosshair to the mouse world location
+			spriteCrosshair.setPosition(mouseWorldPosition);
+
 			// Update the player
 			player.update(dtAsSeconds, Mouse::getPosition());
 			// Make a note of the players new position
@@ -329,6 +338,9 @@ int main()
 
 			// Draw the player
 			window.draw(player.getSprite());
+
+			//Draw the crosshair
+			window.draw(spriteCrosshair);
 		}
 		if (state == State::LEVELING_UP)
 		{
