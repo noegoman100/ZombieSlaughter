4995f00ac863762b00b7829940b412dceb45e8b1
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-08-22 14:38:15 -0600

    Working. Implemented Collisions between player/zombies/pickups

--- Player.cpp
+++ Player.cpp
@@ -45,8 +45,7 @@ Time Player::getLastHitTime()
 }
 bool Player::hit(Time timeHit)
 {
-	if (timeHit.asMilliseconds()
-		- m_LastHit.asMilliseconds() > 200)
+	if (timeHit.asMilliseconds() - m_LastHit.asMilliseconds() > 200)
 	{
 		m_LastHit = timeHit;
 		m_Health -= 10;
